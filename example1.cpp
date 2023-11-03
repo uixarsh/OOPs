63c4a010defd8676ea4dd0a11f6c3099ebaab29d
uixarsh <singhalarsh2001@gmail.com>
2023-11-03 23:22:21 +0530

    Starting OOPs concepts

---
+++ example1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Declaration of a Class.
+class Hero {
+
+    // Defining Properties of a Class.
+    int health;
+};
+
+
+int main () {
+
+    // Creating an Object.
+    Hero h1;
+    cout<<sizeof(h1);
+    cout<<endl;
+
+
+    return 0;
+}
